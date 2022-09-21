115bcf7ae0818c8445eb67cf7f54d43919760132
jhembe <jhembe202@gmail.com>
2022-09-21 13:08:45 +0300

    first arithmetic smthn

---
+++ Arithmetic/src/Arithmetic.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+public class Arithmetic {
+
+    public static void main(String args[]){
+//        System.out.println("Hello world");
+//        System.out.println("Hellow There");
+        int a =14, b=5 ;
+
+        float c = (float)a/b;
+        int  r = a%b;
+
+        System.out.println("c = " + c);
+        System.out.println("r = " + r);
+
+    }
+
+}
